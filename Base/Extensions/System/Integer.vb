a88506934bdbff0ff3cd3b5c2d70812bf5718ffc
Bjorn Falt <n00bl0rd.mail@gmail.com>
2022-05-31 13:00:04 +0100

    Update Integer.vb

--- Base/Extensions/System/Integer.vb
+++ Base/Extensions/System/Integer.vb
@@ -2,6 +2,35 @@
 
     Public Module System_Integer
 
+        <Runtime.CompilerServices.Extension()> Public Function GetWithOrdinalSuffix(i As Integer) As String
+            If i <= 0 Then
+                Return i.ToString()
+            End If
+
+            Dim Value As String = i.ToString()
+
+            If Value.EndsWith("11") Then
+                Return "{0}th".FormatWith(Value)
+            End If
+            If Value.EndsWith("12") Then
+                Return "{0}th".FormatWith(Value)
+            End If
+            If Value.EndsWith("13") Then
+                Return "{0}th".FormatWith(Value)
+            End If
+            If Value.EndsWith("1") Then
+                Return "{0}st".FormatWith(Value)
+            End If
+            If Value.EndsWith("2") Then
+                Return "{0}nd".FormatWith(Value)
+            End If
+            If Value.EndsWith("3") Then
+                Return "{0}rd".FormatWith(Value)
+            End If
+
+            Return "{0}th".FormatWith(Value)
+        End Function
+
         <Runtime.CompilerServices.Extension()> Public Function GetTimeString(seconds As Integer) As String
             Return CLng(seconds).GetTimeString()
         End Function
